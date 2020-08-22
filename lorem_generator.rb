cbda953f79d25b7bdfc8107ca1bb63363988caca
Hugo Arredondo <hugosao22@gmail.com>
2020-08-22 17:01:00 -0400

    add program in lorem_generator.rb

---
+++ lorem_generator.rb
@@ -0,0 +1,8 @@
+num = ARGV[0].to_i
+
+cont = 0
+while cont < num
+    puts "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie felis nec elit euismod, et dapibus sapien sagittis. In justo sapien, rutrum in eros ut, tristique malesuada ante. Aenean et justo risus. Donec in molestie enim, vel porttitor tortor. Nam elementum justo eget blandit consequat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur et lectus vulputate, condimentum orci in, convallis ligula. Fusce lobortis felis et nisi efficitur blandit. Nunc viverra dapibus consectetur."
+    puts
+    cont += 1
+end
