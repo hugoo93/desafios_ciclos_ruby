a99e539aae633e9cda1e008fd9af9b19a07a4278
Hugo Arredondo <hugosao22@gmail.com>
2020-08-22 16:58:36 -0400

    add program in gen.rb

---
+++ gen.rb
@@ -0,0 +1,8 @@
+num = ARGV[0].to_i
+
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in 0..(num-1)
+    print "#{letras[i]}"
+end
+puts
